f9089019774b908c1f55231aeb36d4fbb5ebd590
Mohsin Saleem <junaidbutt123909@gmail.com>
2025-09-05 19:17:01 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Ai-Zuruu-Pharmacy
+# Firebase Studio
+
+This is a NextJS starter in Firebase Studio.
+
+Owner: DR.Mohsin Saleem
+
+To get started, take a look at src/app/page.tsx.
---
+++ next.config.ts
@@ -0,0 +1,31 @@
+
+require('dotenv').config({ path: './.env.local' });
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
